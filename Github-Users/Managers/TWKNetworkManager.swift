eb068c3344012ca111b6df0ec37fd5e5e3ebd01d
William Rena <wilyamx@gmail.com>
2021-02-28 01:49:22 +0800

    method to check if connected to network

--- Github-Users/Managers/TWKNetworkManager.swift
+++ Github-Users/Managers/TWKNetworkManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SystemConfiguration
 
 struct TWKRequestError {
     var errorCode: Int = 0
@@ -24,7 +25,37 @@ class TWKNetworkManager {
     static let BASE_URL = "https://api.github.com"
     static let PAGE_SIZE = 15
     
-    // MARK: - Public Methods
+    // MARK: - Monitoring
+    
+    // https://stackoverflow.com/questions/25398664/check-for-internet-connection-availability-in-swift
+    public func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        if flags.isEmpty {
+            return false
+        }
+
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+
+        return (isReachable && !needsConnection)
+    }
+    
+    // MARK: - GitHub APIs
     
     /*
      [
