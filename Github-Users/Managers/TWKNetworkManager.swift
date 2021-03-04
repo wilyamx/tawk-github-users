b33cbae41782877ed2657dbacbd7725af96a949d
William Rena <wilyamx@gmail.com>
2021-03-04 09:22:05 +0800

    monitor network connection

--- Github-Users/Managers/TWKNetworkManager.swift
+++ Github-Users/Managers/TWKNetworkManager.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import SystemConfiguration
 
 struct TWKRequestError {
     var errorCode: Int = 0
@@ -19,41 +18,12 @@ enum TWKResponse<T> {
     case failed(TWKRequestError)
 }
 
-class TWKNetworkManager {
+class TWKNetworkManager: WSRNetworkMonitor {
+    
     static let shared = TWKNetworkManager()
     
     static let BASE_URL = "https://api.github.com"
     static let PAGE_SIZE = 5
-    
-    // MARK: - Monitoring
-    
-    // https://stackoverflow.com/questions/25398664/check-for-internet-connection-availability-in-swift
-    public func isConnectedToNetwork() -> Bool {
-        var zeroAddress = sockaddr_in()
-        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
-        zeroAddress.sin_family = sa_family_t(AF_INET)
-
-        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
-            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
-                SCNetworkReachabilityCreateWithAddress(nil, $0)
-            }
-        }) else {
-            return false
-        }
-
-        var flags: SCNetworkReachabilityFlags = []
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
-            return false
-        }
-        if flags.isEmpty {
-            return false
-        }
-
-        let isReachable = flags.contains(.reachable)
-        let needsConnection = flags.contains(.connectionRequired)
-
-        return (isReachable && !needsConnection)
-    }
     
     // MARK: - GitHub APIs
     
---
+++ Github-Users/Managers/WSRNetworkMonitor.swift
@@ -0,0 +1,71 @@
+//
+//  WSRNetworkMonitor+Extension.swift
+//  Github-Users
+//
+//  Created by William S. Rena on 3/4/21.
+//  Copyright © 2021 Tawk.to. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+class WSRNetworkMonitor {
+    private var monitoringTimer: Timer?
+    private var lastConnectionStatus: Bool = false
+    
+    // MARK: - Monitoring
+    
+    // https://stackoverflow.com/questions/25398664/check-for-internet-connection-availability-in-swift
+    
+    public func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        if flags.isEmpty {
+            return false
+        }
+
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+
+        return (isReachable && !needsConnection)
+    }
+    
+    public func startMonitoringNetworkConnection() {
+        self.lastConnectionStatus = self.isConnectedToNetwork()
+        self.monitoringTimer = Timer.scheduledTimer(
+            withTimeInterval: 1.0,
+            repeats: true) {
+            timer in
+            
+            let newConnectionStatus = self.isConnectedToNetwork()
+            if newConnectionStatus != self.lastConnectionStatus {
+                self.lastConnectionStatus = newConnectionStatus
+                
+                let notificationName = Notification.Name.init(rawValue: "WSRNetworkMonitor.connectionChange")
+                let notificationCenter: NotificationCenter = .default
+                notificationCenter.post(name: notificationName, object: nil)
+            }
+          }
+    }
+    
+    public func stopMonitoringNetworkConnection() {
+        if let timer = self.monitoringTimer {
+            timer.invalidate()
+            self.monitoringTimer = nil
+        }
+    }
+}
